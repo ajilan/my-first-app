7efed714e805ceaae9df2c6c2ee43008088346e0
ajilan <ajilann123@gmail.com>
2019-01-29 00:06:19 -0800

    event added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'no server was created'
   constructor() { 
     setTimeout(() => {
     this.allowNewServer = true;  
@@ -17,6 +18,15 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {
+
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'server was created';
+  }
 }
+
+
+
+
+
